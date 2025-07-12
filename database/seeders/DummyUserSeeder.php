7eab924085c3d1cb29b10c817fb7db193ded5542
Muhammad Ludvi Argorahayu <muhammadludvi468@gmail.com>
2025-07-12 22:57:12 +0700

    commit untuk deplyo jaga jaga aja

--- database/seeders/DummyUserSeeder.php
+++ database/seeders/DummyUserSeeder.php
@@ -14,15 +14,20 @@ class DummyUserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([ 
+        /* User::create([ 
             'name' => 'Admin', 
             'email' => 'admin@example.com', 
             'password' => Hash::make('password') 
-        ]);
+        ]); */
         /* User::create([ 
             'name' => 'Argorahayu', 
             'email' => 'pbw@gmail.com', 
             'password' => Hash::make('2ks3') 
         ]); */
+        User::create([ 
+            'name' => 'stis', 
+            'email' => 'stis@example.com', 
+            'password' => Hash::make('setis') 
+        ]);
     }
 }
