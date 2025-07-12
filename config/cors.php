5b1e86f673c8dfdf3aa249e0e890ac82207f12b5
Muhammad Ludvi Argorahayu <muhammadludvi468@gmail.com>
2025-07-12 23:26:35 +0700

    Fixing CORS 2

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'https://aplikasi-web-bps-frontend.vercel.app'],
+    'allowed_origins' => ['http://localhost:5173', 'https://aplikasi-web-bps-backend-production.up.railway.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
